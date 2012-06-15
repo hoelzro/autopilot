43556495cde88e7b965cc992dc2eb81d678c4e01
Rob Hoelz <rob@hoelz.ro>
2012-06-15 11:57:47 -0500

    Move import into Lua

--- src/config_environment.lua
+++ src/config_environment.lua
@@ -1,2 +1,14 @@
 local env = ...
 
+local require = env.require
+
+function env.import(name)
+  local result    = require(name)
+  local callerenv = getfenv(2)
+
+  for k, v in pairs(result) do
+    callerenv[k] = v
+  end
+
+  return result
+end
