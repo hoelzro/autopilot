a768005e2baec25157b2dac43bfe27544802ac75
Rob Hoelz <rob@hoelz.ro>
2012-06-15 13:54:25 -0500

    Implement our own require

--- src/config_environment.lua
+++ src/config_environment.lua
@@ -1,5 +1,31 @@
 local env = ...
 
+function env.require(name)
+  local cached = package.loaded[name]
+
+  if cached then
+    return cached
+  end
+
+  local loaders = package.loaders
+  local errors  = {}
+
+  for i, v in ipairs(loaders) do
+    local result = loaders[i](name)
+    local t      = type(result)
+
+    if t == 'function' then
+      local childenv = setmetatable({}, { __index = env })
+      setfenv(result, childenv)
+      return result(name)
+    elseif t == 'string' then
+      errors[#errors + 1] = result
+    end
+  end
+
+  error(table.concat(errors, '\n'), 2)
+end
+
 local require = env.require
 
 function env.import(name)
