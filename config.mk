b0d31fa7b4dc834783c24e518b9abbf59113a66b
Rob Hoelz <rob@hoelz.ro>
2012-06-24 17:28:14 -0500

    Add PREFIX and DESTDIR

--- config.mk
+++ config.mk
@@ -1,2 +1,5 @@
+PREFIX=/usr/local
+DESTDIR=/usr/local
+
 CFLAGS+=-g -I $(ROOT)/include -DCOMPILING_AUTOPILOT=1 -fPIC -Wall -Wimplicit -Wimplicit-function-declaration -Wimplicit-int
 LIBS+=-levent -ldl -llua -lm
